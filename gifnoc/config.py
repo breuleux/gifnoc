0567f67d452fbb436776ce519496599276df9338
Olivier Breuleux <breuleux@gmail.com>
2024-01-18 18:36:21 +0100

    Make subconfigurations importable

--- gifnoc/config.py
+++ gifnoc/config.py
@@ -1,1 +1,32 @@
-from .core import get as __getattr__  # noqa: F401
+from .core import active_configuration
+
+
+class _Proxy:
+    def __init__(self, *pth):
+        self._pth = pth
+
+    def __obj(self):
+        cfg = active_configuration.get()
+        if cfg is None:
+            raise Exception("No configuration was loaded.")
+        try:
+            cfg = cfg.built
+            for k in self._pth:
+                if isinstance(cfg, dict):
+                    cfg = cfg[k]
+                else:
+                    cfg = getattr(cfg, k)
+            return cfg
+        except (KeyError, AttributeError):
+            key = ".".join(self._pth)
+            raise Exception(f"No configuration was loaded for key '{key}'.")
+
+    def __getattr__(self, attr):
+        return getattr(self.__obj(), attr)
+
+
+def __getattr__(key):
+    return _Proxy(key)
+
+
+__path__ = None
