956d3c7f0fc5df4c1827da83304aa47a72d7983d
Olivier Breuleux <breuleux@gmail.com>
2024-01-19 12:47:59 +0100

    Document gifnoc function

--- gifnoc/core.py
+++ gifnoc/core.py
@@ -104,26 +104,60 @@ def create_arg(model: Union[int, float, str, Path], info: Info):  # noqa: F811
 def gifnoc(
     envvar="APP_CONFIG",
     config_argument="--config",
-    default_source=None,
     sources=[],
     option_map={},
     environ_map=default_environ_map,
     environ=os.environ,
     argparser=None,
     parse_args=True,
-    argv=[],
+    argv=None,
     write_back_environ=True,
 ):
+    """Context manager to find/assemble configuration for the code within.
+
+    All configuration and configuration files specified through environment
+    variables, the command line, and the sources parameter will be merged
+    together.
+
+    Arguments:
+        envvar: Name of the environment variable to use for the path to the
+            configuration. (default: "APP_CONFIG")
+        config_argument: Name of the command line argument used to specify
+            one or more configuration files. (default: "--config")
+        sources: A list of Path objects and/or dicts that will be merged into
+            the final configuration.
+        option_map: A map from command-line arguments to configuration paths,
+            for example ``{"--port": "server.port"}`` will add a ``--port``
+            command-line argument that will set ``gifnoc.config.server.port``.
+        environ_map: A map from environment variables to configuration paths,
+            for example ``{"SERVER_PORT": "server.port}`` will set
+            ``gifnoc.config.server.port`` to the value of the ``$SERVER_PORT``
+            environment variable. By default this is the global map in
+            ``gifnoc.registry.envmap``, which most of the time is what you want,
+            so there is usually no need to provide this argument.
+        environ: The environment variables, by default ``os.environ``.
+        argparser: The argument parser to add arguments to. If None, an
+            argument parser will be created.
+        parse_args: Whether to parse command-line arguments.
+        argv: The list of command-line arguments.
+        write_back_environ: If True, the mappings in ``environ_map`` will be used
+            to write the configuration into ``environ``, for example if environ_map
+            is ``{"SERVER_PORT": "server.port}``, we will set
+            ``environ["SERVER_PORT"] = gifnoc.config.server.port`` after parsing
+            the configuration. (default: True)
+    """
+
     if parse_args:
         if argparser is None:
             argparser = ArgumentParser()
-        argparser.add_argument(
-            config_argument,
-            dest="$config",
-            metavar="CONFIG",
-            action="append",
-            help="Configuration file(s) to load.",
-        )
+        if config_argument:
+            argparser.add_argument(
+                config_argument,
+                dest="$config",
+                metavar="CONFIG",
+                action="append",
+                help="Configuration file(s) to load.",
+            )
 
         model = global_model()
         for opt, path in option_map.items():
@@ -135,13 +169,12 @@ def gifnoc(
                 typ, Info(argparser=argparser, help=hlp, opt=main, aliases=aliases)
             )
 
-        options = argparser.parse_args(argv or sys.argv[1:])
+        options = argparser.parse_args(sys.argv[1:] if argv is None else argv)
     else:
         options = SimpleNamespace(config=[])
 
     sources = [
         environ.get(envvar, None),
-        default_source,
         *sources,
         *(getattr(options, "$config") or []),
         EnvironMap(environ=environ, map=environ_map),
