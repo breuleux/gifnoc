7be670e7b0eaded2b232b4b857987476eaab8cf3
Olivier Breuleux <breuleux@gmail.com>
2024-01-30 13:11:15 -0500

    Add define function

--- gifnoc/__init__.py
+++ gifnoc/__init__.py
@@ -4,3 +4,10 @@ from .registry import (  # noqa: F401
     map_environment_variables,
 )
 from . import config  # noqa: F401
+
+
+def define(field, model, environ=None):
+    register(field, model)
+    if environ:
+        map_environment_variables(**{k: f"{field}.{v}" for k, v in environ.items()})
+    return getattr(config, field)
