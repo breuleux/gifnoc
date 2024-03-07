e0e8bf5b07218c96461c43e23da307bf1ed1fa90
Olivier Breuleux <breuleux@gmail.com>
2024-03-07 15:13:15 -0500

    Move define function to its own file

--- gifnoc/__init__.py
+++ gifnoc/__init__.py
@@ -1,4 +1,3 @@
-from typing import Optional, Type, TypeVar
 
 from . import config  # noqa: F401
 from .core import (  # noqa: F401
@@ -13,18 +12,8 @@ from .core import (  # noqa: F401
     overlay,
     use,
 )
+from .define import define  # noqa: F401
 from .registry import (  # noqa: F401
     map_environment_variables,
     register,
 )
-
-_T = TypeVar("_T")
-
-
-def define(field: str, model: Type[_T], environ: Optional[dict] = None) -> _T:
-    # The typing is a little bit of a lie since we're returning a _Proxy object,
-    # but it works just the same.
-    register(field, model)
-    if environ:
-        map_environment_variables(**{k: f"{field}.{v}" for k, v in environ.items()})
-    return getattr(config, field)
