94e6496e488e7e2d15eaac3b8b7de4454b3128c6
Olivier Breuleux <breuleux@gmail.com>
2024-04-03 20:03:23 -0400

    Test config and proxy

--- gifnoc/proxy.py
+++ gifnoc/proxy.py
@@ -1,6 +1,10 @@
 from .interface import current_configuration
 
 
+class MissingConfigurationError(Exception):
+    pass
+
+
 class Proxy:
     def __init__(self, *pth):
         self._pth = pth
@@ -9,8 +13,8 @@ class Proxy:
 
     def _obj(self):
         container = current_configuration()
-        if container is None:
-            raise Exception("No configuration was loaded.")
+        if container is None:  # pragma: no cover
+            raise MissingConfigurationError("No configuration was loaded.")
         root = cfg = container.built
         if cfg is self._cached_built:
             return self._cached
@@ -18,23 +22,24 @@ class Proxy:
             for k in self._pth:
                 if isinstance(cfg, dict):
                     cfg = cfg[k]
+                elif isinstance(cfg, list):
+                    cfg = cfg[int(k)]
                 else:
                     cfg = getattr(cfg, k)
-            if cfg is None:
-                key = ".".join(self._pth)
-                raise Exception(f"No configuration was loaded for key '{key}'.")
             self._cached_built = root
             self._cached = cfg
             return cfg
         except (KeyError, AttributeError):
             key = ".".join(self._pth)
-            raise Exception(f"No configuration was loaded for key '{key}'.")
+            raise MissingConfigurationError(
+                f"No configuration was loaded for key '{key}'."
+            )
 
     def __str__(self):
         return f"Proxy for {self._obj()}"
 
     def __repr__(self):
-        return f"_Proxy({self._obj()!r})"
+        return f"Proxy({self._obj()!r})"
 
     def __getattr__(self, attr):
         if attr.startswith("__") and attr.endswith("__"):
