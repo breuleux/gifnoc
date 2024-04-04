e5bebbb4285b6e7cb16ea637bf0a4e91a8f02ed4
Olivier Breuleux <breuleux@gmail.com>
2024-04-03 21:05:55 -0400

    Test registry and acquire

---
+++ tests/test_acquire.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from gifnoc.acquire import EnvContext, FileContext, acquire
+
+from .models import City, Person
+
+
+def test_acquire_file():
+    acq = acquire(
+        City,
+        Path(__file__).parent / "objects" / "person-links.yaml",
+        FileContext(path=None),
+    )
+    assert acq == {
+        "people": [{"name": "Olivier", "age": 39}, {"name": "Sophie", "age": 31}]
+    }
+
+
+def test_acquire_environment():
+    acq = acquire(Person, {"name": "bob", "age": "41", "fabulous": "1"}, EnvContext())
+    assert acq == {"name": "bob", "age": 41, "fabulous": True}
--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,3 +1,8 @@
+from dataclasses import dataclass
+
+from .models import Point
+
+
 def test_use(org, registry, configs):
     with registry.use(configs / "mila.yaml"):
         assert org.name == "mila"
@@ -11,3 +16,40 @@ def test_refresh(org, registry, configs):
         assert org.name == "google"
         cfg.refresh()
         assert org.name == "microsoft"
+
+
+def test_context(registry):
+    sentinel = [-1]
+
+    @dataclass
+    class Fudge:
+        deliciousness: int
+
+        def __enter__(self):
+            sentinel[0] = self.deliciousness
+
+        def __exit__(self, ext, exv, tb):
+            sentinel[0] = -1
+
+    registry.define(field="fudge", model=Fudge)
+
+    assert sentinel[0] == -1
+    with registry.use({"fudge": {"deliciousness": 666}}):
+        assert sentinel[0] == 666
+    assert sentinel[0] == -1
+
+
+def test_register_deep(registry):
+    p1 = registry.define(
+        field="points.one",
+        model=Point,
+    )
+    p2 = registry.define(
+        field="points.two",
+        model=Point,
+    )
+    with registry.use({"points": {"one": Point(1, 2), "two": Point(10, 20)}}):
+        assert p1.x == 1
+        assert p1.y == 2
+        assert p2.x == 10
+        assert p2.y == 20
